420edbc0121cac2686832697bf3040f07cab60af
S3NP41-v <s3np41v@gmail.com>
2023-07-09 21:17:40 +0200

    Suggested Changes

--- hackernews_bot.py
+++ hackernews_bot.py
@@ -2,21 +2,34 @@ import discord
 import requests
 from bs4 import BeautifulSoup
 
-TOKEN = 'YOUR_DISCORD_BOT_TOKEN'  # Replace with your bot token
-CHANNEL_ID = 1234567890  # Replace with the ID of the channel to send the news
+# added json for reading config.json
+from json import loads
 
-client = discord.Client()
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user.name}')
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('!hackernews'):
-        await fetch_hacker_news()
+class Bot(discord.Client):
+    async def on_ready(self):
+        # this honestly is not the best approach, but i have yet to find a better one...
+        # `on_ready` should not really be used like `__init__`
+        self.config = getConfig()
+        print(f'We have logged in as {self.user.name}')
 
-async def fetch_hacker_news():
+    async def on_message(self, message):
+        # ignoring messages from self or other bots
+        if message.author.bot:
+            return
+
+        if message.content.startswith('!hackernews'):
+            news_embed = await fetch_hacker_news()
+
+            # Moved the sending into `on_message` for name clarity `fetch_...` should not perform sending
+            # further improvement would be to write methods in a seperate file, and as the scope increases
+            # a command parser may be necesarry
+            channel = self.get_channel(self.config["news-channel"])
+            await channel.send(embed=news_embed)
+
+# In testing i was unable to get any news, maybe needs fixing?
+async def fetch_hacker_news() -> discord.Embed:
     url = 'https://news.ycombinator.com/'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -27,8 +40,37 @@ async def fetch_hacker_news():
         title = item.get_text()
         link = item['href']
         news_embed.add_field(name=title, value=f'[Read more]({link})', inline=False)
+    
+    return news_embed
 
-    channel = client.get_channel(CHANNEL_ID)
-    await channel.send(embed=news_embed)
 
-client.run(TOKEN)
+def getConfig() -> dict:
+    try:
+        with open("config.json", 'r') as f:
+            configData = loads(f.read())
+
+    except FileNotFoundError:
+        print("Could not find file config.json")
+        exit()
+
+    except PermissionError:
+        print("Could not read file config.json (Permissions missing?)")
+        exit()
+
+    else:
+        return configData
+
+
+# added main and run condition
+def main():
+    # intents should be specified
+    intents = discord.Intents.default()
+    intents.messages = True
+    intents.message_content = True
+
+    client = Bot(intents=intents)
+    client.run(getConfig()["token"])
+
+# in case something wants to import from this file
+if __name__ == "__main__":
+    main()
